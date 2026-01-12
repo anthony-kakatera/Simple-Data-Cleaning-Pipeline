4b6d35ac07f364153f9be588f0f7167cd65fb8f0
anthony-kakatera <anthonyckakatera@gmail.com>
2026-01-12 10:55:11 +0200

    Add validate_email function to utils.py

--- utils.py
+++ utils.py
@@ -6,3 +6,9 @@ def clean_names(name):
     if pd.isna(name):
         return None
     return name.strip().title()
+
+def validate_email(email):
+    if pd.isna(email):
+        return False
+    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    return bool(re.match(pattern, email))
